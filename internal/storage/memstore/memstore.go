1b2a2e4d51561965deda743c7f985e7362bd5b8c
Gennady Badinov <gennady.badinov@gmail.com>
2023-04-07 22:36:21 +0300

    fix: Clean up the code

--- internal/storage/memstore/memstore.go
+++ internal/storage/memstore/memstore.go
@@ -4,6 +4,11 @@ import (
 	"errors"
 )
 
+var (
+	ErrorURLNotFound   = errors.New("wrong hash provided")
+	ErrorNoURLProvided = errors.New("no url provided")
+)
+
 type Storage struct {
 	data map[string]string
 }
@@ -18,12 +23,12 @@ func (s *Storage) Read(key string) (string, error) {
 	if value, ok := s.data[key]; ok {
 		return value, nil
 	}
-	return "", errors.New("wrong hash")
+	return "", ErrorURLNotFound
 }
 
 func (s *Storage) Write(key string, value string) error {
 	if key == "" {
-		return errors.New("no key provided")
+		return ErrorNoURLProvided
 	}
 	s.data[key] = value
 	return nil
