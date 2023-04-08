f8135db9952d0be26745651a26aac58c62d4f72f
Gennady Badinov <gennady.badinov@gmail.com>
2023-04-08 14:37:59 +0300

    test: Add invalid data tests

--- internal/app/app_test.go
+++ internal/app/app_test.go
@@ -23,18 +23,36 @@ func TestShortenURL(t *testing.T) {
 	server := app.New(storage)
 	server.MountHandlers()
 
+	// Correct long URL
 	shortenReq, _ := http.NewRequest(http.MethodPost, "/", bytes.NewBufferString(longURL))
 	shortenResp := httptest.NewRecorder()
 	server.Router.ServeHTTP(shortenResp, shortenReq)
-
 	shortURL, _ := url.ParseRequestURI(shortenResp.Body.String())
 	urlHash := shortURL.Path[1:]
 
-	assert.Equal(t, shortenResp.Code, http.StatusCreated)
-	assert.Equal(t, shortenResp.Header().Get(app.ContentType), app.PlaingText)
-	assert.Equal(t, shortURL.Host, config.Addr)
-	assert.Equal(t, len(urlHash), config.HashLen)
+	assert.Equal(t, http.StatusCreated, shortenResp.Code)
+	assert.Equal(t, app.PlaingText, shortenResp.Header().Get(app.ContentType))
+	assert.Equal(t, config.Addr, shortURL.Host)
+	assert.Equal(t, config.HashLen, len(urlHash))
 	assert.Regexp(t, hashPattern, urlHash)
+
+	// Empty string
+	emptyStringReq, _ := http.NewRequest(http.MethodPost, "/", bytes.NewBufferString(""))
+	emptyStringResp := httptest.NewRecorder()
+	server.Router.ServeHTTP(emptyStringResp, emptyStringReq)
+
+	assert.Equal(t, http.StatusBadRequest, emptyStringResp.Code)
+	assert.Equal(t, app.PlaingText, shortenResp.Header().Get(app.ContentType))
+	assert.Equal(t, "Invalid URL\n", emptyStringResp.Body.String())
+
+	// Invalid URL
+	invalidUrlReq, _ := http.NewRequest(http.MethodPost, "/", bytes.NewBufferString("123"))
+	invalidUrlResp := httptest.NewRecorder()
+	server.Router.ServeHTTP(invalidUrlResp, invalidUrlReq)
+
+	assert.Equal(t, http.StatusBadRequest, invalidUrlResp.Code)
+	assert.Equal(t, "Invalid URL\n", invalidUrlResp.Body.String())
+
 }
 
 func TestExpandURL(t *testing.T) {
@@ -42,10 +60,10 @@ func TestExpandURL(t *testing.T) {
 	server := app.New(storage)
 	server.MountHandlers()
 
+	// Correct Hash
 	shortenReq, _ := http.NewRequest(http.MethodPost, "/", bytes.NewBufferString(longURL))
 	shortenResp := httptest.NewRecorder()
 	server.Router.ServeHTTP(shortenResp, shortenReq)
-
 	shortURL, _ := url.ParseRequestURI(shortenResp.Body.String())
 	urlHash := shortURL.Path[1:]
 
@@ -53,6 +71,15 @@ func TestExpandURL(t *testing.T) {
 	expandResp := httptest.NewRecorder()
 	server.Router.ServeHTTP(expandResp, expandReq)
 
-	assert.Equal(t, expandResp.Code, http.StatusTemporaryRedirect)
-	assert.Equal(t, expandResp.Header().Get(app.Location), longURL)
+	assert.Equal(t, http.StatusTemporaryRedirect, expandResp.Code)
+	assert.Equal(t, longURL, expandResp.Header().Get(app.Location))
+
+	// Invalid Hash
+	invalidHashReq, _ := http.NewRequest(http.MethodGet, "/"+"invalidHash", nil)
+	invalidHashResp := httptest.NewRecorder()
+	server.Router.ServeHTTP(invalidHashResp, invalidHashReq)
+
+	assert.Equal(t, http.StatusBadRequest, invalidHashResp.Code)
+	assert.Equal(t, "", invalidHashResp.Header().Get(app.Location))
+
 }
