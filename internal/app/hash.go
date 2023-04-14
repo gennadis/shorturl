1c176a3919b980a1e4eebbadeda2d962bd6b87ec
Gennady Badinov <gennady.badinov@gmail.com>
2023-04-14 18:54:41 +0300

    refactor: Remove excessive hash string formatting

--- internal/app/hash.go
+++ internal/app/hash.go
@@ -3,7 +3,6 @@ package app
 import (
 	"crypto/md5"
 	"fmt"
-	"strings"
 
 	"github.com/gennadis/shorturl/config"
 )
@@ -11,6 +10,6 @@ import (
 // TODO: Use more data to generate hash
 func GenerateHash(s string, n int) string {
 	checksum := md5.Sum([]byte(s))
-	hash := fmt.Sprintf("%X", checksum)
-	return strings.ToLower(hash[0:config.HashLen])
+	hash := fmt.Sprintf("%x", checksum)
+	return hash[0:config.HashLen]
 }
